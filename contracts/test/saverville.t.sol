b7b3a64b79806561973d4ab9edae7e38530a851b
Raphael Nembhard <raphaelnembhard@Raphaels-MacBook-Pro.local>
2024-05-08 18:06:34 -0400

    adding contracts

---
+++ contracts/test/saverville.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Saverville} from "../src/saverville.sol";
+
+contract SavervilleTest is Test {
+    Saverville public saverville;
+
+    function setUp() public {
+        saverville = new Saverville();
+    }
+
+    function createCropTest() public {
+
+    }
+
+    function depositTest() public {
+
+    }
+
+    function harvestTest() public {
+        
+    }
+
+}
